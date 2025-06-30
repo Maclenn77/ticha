af966b4b86b09ca4932368bf9be1171535ddf642
J.P. Pérez Tejada <juanpapas@comunidad.unam.mx>
2025-06-29 23:00:52 -0600

    bump version 0.1.0 -> 0.2.0

--- src/ticha/__init__.py
+++ src/ticha/__init__.py
@@ -1,6 +1,6 @@
 """Ticha Colonial Zapotec manuscript scraper."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .core.scraper import TichaScraper
 from .core.text_scraper import TichaTextScraper
